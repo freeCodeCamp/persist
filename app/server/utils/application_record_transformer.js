8aec689e90df4b0433ee4dfa92149a816ff55d6a
Sachin Mour <rtr.sachinmour@gmail.com>
2017-01-04 17:10:24 -0500

    upload applications server

---
+++ app/server/utils/application_record_transformer.js
@@ -0,0 +1,31 @@
+import College from '../models/college';
+
+export default (record, callback) => {
+    if (!record.osis) {
+        callback(null, null);
+        return;
+    }
+
+//  reference college
+    College.findOne({
+        $or: [
+            {fullName: record.college},
+            {shortName: record.college},
+            {navianceName: record.college},
+            {collegeScorecardName: record.college}
+        ]
+    }, (err, college) => {
+        if (err) {
+            console.log('college not found', err);
+            callback(err);
+            return;
+        }
+        if (college) {
+            record.college = college._id;
+            callback(null, record);
+            return;
+        }
+        callback(null, null);
+    });
+
+}
--- app/server/utils/save_csv_applications.js
+++ app/server/utils/save_csv_applications.js
@@ -1,136 +1,112 @@
 import fs from 'fs';
 import parse from 'csv-parse';
 import transform from 'stream-transform';
-import async from 'async';
-import mongoose from 'mongoose';
+import async from 'async'
+import winston from 'winston';
+import _ from 'lodash';
+
 import Student from '../models/student';
-
-// this is the 
 import {applicationKeys} from '../../common/fieldKeys';
+import formatRecord from './application_record_transformer';
 
-
-export default function (fileName) {
-
-    return new Promise((resolve, reject) => {
-
-        var parser = parse({
-            delimiter: ',',
-            columns: mapValues,
-            auto_parse: true
-        });
-
-        var transformer = transform(function (record) {
-
-            console.log(record);
-
-        }, (err, data) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            // reduce data size for testing
-            // data = data.splice(0, 10);
-            let addedCount = 0;
-            let modifiedCount = 0;
-            let newStudents = [];
-            let updatedStudents = [];
-
-            console.time('dbSave');
-
-            async.eachLimit(data, 10, (record, callback) => {
-
-                // find student record
-
-
-                // if exists - modify values in a set way for each value
-
-                Student.findOne({
-                    osis: record.osis
-                }, (err, doc) => {
-
-                    // if doesnt exist - create new record
-                    if (!doc) {
-                        var student = new Student(record);
-                        student.save((err, doc) => {
-                            if (err) {
-                                callback(err);
-                                return;
-                            }
-                            addedCount++;
-                            newStudents.push({osis: doc.osis, firstName: doc.firstName, lastName: doc.lastName});
-                            callback(null);
-                        });
-                    } else {
-                        modifiedCount++;
-                        console.log('the record exists already mate!'.red)
-                        updatedStudents.push({
-                            osis: record.osis,
-                            firstName: record.firstName,
-                            lastName: record.lastName
-                        })
-                        // run some logic of updating
-
-                        // update document with rules from Molly
-                        // options: overwrite / add
-
-
-                        callback(null);
-                    }
-
-                });
-
-                // Student.update({
-                //   osis: record.osis
-                // }, record, {
-                //   upsert: true,
-                //   setDefaultsOnInsert: true
-                // }, (err, rawResponse) => {
-                //   if (err) {
-                //     callback(err);
-                //     return;
-                //   }
-
-                //   if (rawResponse.upserted) {
-                //     addedCount += 1;
-                //   } else {
-                //     modifiedCount += 1;
-                //   }
-                //   callback(null);
-
-                // });
-
-            }, (err) => {
-                console.log('getting through to here'.blue)
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                console.timeEnd('dbSave');
-                resolve({
-                    modifiedCount,
-                    addedCount,
-                    newStudents,
-                    updatedStudents
-                });
-            });
-        });
-
-        fs.createReadStream(fileName).pipe(parser).pipe(transformer);
-
-    });
-}
-
-
-function mapValues(line) {
-
+const mapValues = (line) => {
     return line.map((key) => {
-        var obj = applicationKeys.find((field) => {
+        const obj = applicationKeys.find((field) => {
             return field.fieldName === key;
         });
         if (obj) {
             return obj.dbName;
         }
-        return key;
+        return null;
+    });
+};
+
+export default (fileName) => {
+    return new Promise((resolve, reject) => {
+
+        const parser = parse({
+            delimiter: ',',
+            columns: mapValues,
+            auto_parse: true
+        });
+
+        const transformer = transform(formatRecord, {
+            parallel: 20
+        });
+        const data = {};
+        let row;
+        transformer.on('readable', () => {
+            while (row = transformer.read()) {
+                data[row.osis] = data[row.osis] || [];
+                data[row.osis].push(row);
+            }
+        });
+        let error;
+        transformer.on('error', (err) => {
+            error = err;
+            console.log(err.message);
+        });
+
+        transformer.on('end', () => {
+            console.log(data[274724103]);
+            return reject(null);
+            if (error) return reject(error);
+            async.eachLimit(data, 10, (applications, callback) => {
+                if (!applications || applications.length < 1) {
+                    callback(null);
+                    return;
+                }
+                const osis = applications[0].osis;
+                // find student record
+                // if exists - only then proceed
+                Student.findOne({
+                    osis
+                }, (err, student) => {
+                    if (err) {
+                        callback(err);
+                        return;
+                    }
+                    if (student) {
+                        let studentApplications = student.applications;
+                        applications.forEach((applicationRecord) => {
+                            let application = studentApplications.find((elem) => {
+                                return elem.college.toString() === applicationRecord.college.toString();
+                            });
+                            if (application) {
+                                _.merge(application, applicationRecord);
+                            } else {
+                                studentApplications.push(applicationRecord);
+                            }
+                        });
+                        studentApplications = studentApplications.filter((obj) =>
+                            (!_.isEmpty(obj))
+                        );
+                        studentApplications = _.sortBy(studentApplications, (obj) => {
+                            return obj.enrolBegin;
+                        }).reverse();
+                        student.applications = studentApplications;
+                        // for now, lets just overwrite the doc
+                        student.save((err, updatedStudent) => {
+                            if (err) {
+                                console.log('error', studentApplications, student);
+                                callback(err);
+                            } else {
+                                callback(null);
+                            }
+                        });
+                    } else {
+                        return callback(null);
+                    }
+                });
+            }, (err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(true);
+            });
+        });
+
+        fs.createReadStream(fileName).pipe(parser).pipe(transformer);
     });
 }
