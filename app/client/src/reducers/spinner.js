f6474be62022f35d6a34f988e08b2d80bbf17f58
Sachin Mour <rtr.sachinmour@gmail.com>
2017-01-23 10:03:57 +0530

    bug

--- app/client/src/reducers/spinner.js
+++ app/client/src/reducers/spinner.js
@@ -3,7 +3,6 @@ import {SPINNER} from '../actions/types';
 const defaultState = true;
 
 export default function (state = defaultState, action) {
-    console.log(action);
     switch (action.type) {
         case SPINNER:
             return action.payload;
