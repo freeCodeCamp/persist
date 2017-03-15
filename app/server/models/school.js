c1b6f456dddbcd53d09945325a96c29c28a581d2
Sachin Mour <rtr.sachinmour@gmail.com>
2017-03-15 22:05:18 +0100

    roll back school names

--- app/server/models/school.js
+++ app/server/models/school.js
@@ -6,8 +6,8 @@ const Schema = mongoose.Schema;
 
 const schoolSchemaModel = new Schema(schoolSchema(Schema));
 
-const schoolNames = ['Baldwin', 'Brooklyn Collaborative', 'Channel View', 'Kurt Hahn',
-    'Leaders', 'Gaynor McCown', 'MELS', 'WHEELS'];
+const schoolNames = ['Baldwin', 'BCS', 'Channel View', 'Hahn',
+    'Leaders', 'McCown', 'MELS', 'WHEELS'];
 const School = mongoose.model('School', schoolSchemaModel);
 
 async.each(schoolNames, (schoolName, callback) => {
