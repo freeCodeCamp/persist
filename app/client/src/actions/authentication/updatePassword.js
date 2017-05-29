e4ccf814e727a6e4f1bc1bf7fd8ac3a6572377c1
Tyler Collins <tjscollins@gmail.com>
2017-05-29 11:28:12 +1000

    These actions need error handling.  A decision needs to be made about how to handle those errors (i.e. whether or not to notify the user)

--- app/client/src/actions/authentication/updatePassword.js
+++ app/client/src/actions/authentication/updatePassword.js
@@ -6,6 +6,8 @@ const updatePassword = (token, { password }) =>
         dispatch({
             type: UPDATE_PASSWORD_PENDING
         });
+        // FIXME Needs to handle 404 or other failed status.  Unit test fails presently because
+        // there is no callback for a failure, causing unit test to time out waiting for response.
         return axios.post(`/update-password/${token}`, { password }).then(response => {
             dispatch({
                 type: UPDATE_PASSWORD_SUCCESS
