bfdf46a4be4e93424d074498cdae3f77938bedfd
Tyler Collins <tjscollins@gmail.com>
2017-06-07 13:22:00 +1000

    Added two tests, removed tests checking enums as unnecessary

--- test/server/test_models/student_test.js
+++ test/server/test_models/student_test.js
@@ -3,17 +3,48 @@ const sinon = require('sinon');
 const path = require('path');
 const {ObjectID} = require('mongodb');
 const validator = require('../../../app/common/validator');
+const {map} = require('lodash');
 
 const dbModels = require(path.join(process.env.PWD, 'app/server/models'));
 
 describe('student', () => {
   const {enums} = validator;
 
-  it.skip('should only allow an ethnicity integer in range [1,7]', () => {});
+  it('should require a unique osis value', (done) => {
+    const validStudents = [
+      { osis: 1 },            // Unused value
+    ];
+    const invalidStudents = [
+      { osis: 1 },            // Duplicate value
+    ];
 
-  it.skip('should only allow gender from enums.gender', () => {});
+    const saveStudent = student => new dbModels.Student(student).save();
 
-  it.skip('should only allow postSecPlan from enums.postSecPlan', () => {});
+    Promise.all(validStudents.map(saveStudent))
+      .then(() => Promise.all(invalidStudents.map(saveStudent))
+                    .then(success => done(new Error(`Student model should not save ${success}`)))
+                    .catch(err => done()))
+      .catch(done);
+  });
+
+  it('should only allow an ethnicity integer in range [1,7]', (done) => {
+    const validStudents = [
+      { ethnicity: 1, osis: 1 },
+      { ethnicity: 7, osis: 2 },
+    ];
+    const invalidStudents = [
+      { ethnicity: 0, osis: 3 },
+      { ethnicity: 8, osis: 4 },
+    ];
+
+    const saveStudent = student => new dbModels.Student(student).save();
+
+    Promise.all(map(validStudents, saveStudent))
+      .then(() => Promise.all(map(invalidStudents, saveStudent))
+                    .then(success => done(new Error(`Student model should not save ${success}`)))
+                    .catch(err => done()))
+      .catch(done);
+  });
 
   it.skip('should validate the descriptors field', () => {});
 
@@ -21,29 +52,7 @@ describe('student', () => {
 
   it.skip('should validate the employmentStatus field', () => {});
 
-  it.skip('should only allow progressToGradAss from enums.progressToGradAss', () => {});
-
-  it.skip('should only allow progressToGradBa from enums.progressToGradBa', () => {});
-
   it.skip('should validate the remediationStatus field', () => {});
 
   it.skip('should validate the transferStatus field', () => {});
-
-  it.skip('should only allow commonApp from enums.commonApp', () => {});
-
-  it.skip('should only allow cunyApp from enums.cunyApp', () => {});
-
-  it.skip('should only allow registeredForClasses from enums.registeredForClasses', () => {});
-
-  it.skip('should only allow residency from enums.residency', () => {});
-
-  it.skip('should only allow sunyApp from enums.sunyApp', () => {});
-
-  it.skip('should only allow tshirtSize from enums.tshirtSize', () => {});
-
-  it.skip('should only allow sweatshirtSize from enums.sweatshirtSize', () => {});
-
-  it.skip('should only allow housingStatus from enums.housingStatus', () => {});
-
-  it.skip('should only allow physImmunRecords from enums.physImmunRecords', () => {});
 });
