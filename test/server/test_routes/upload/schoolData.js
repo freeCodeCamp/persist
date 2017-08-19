e307fabeed0facd290acaf7c277445a55ee18fc8
Tyler Collins <tjscollins@gmail.com>
2017-08-19 15:04:21 +1000

    Tests for error handling for collegeData, schoolData, termData upload routes

--- test/server/test_routes/upload/schoolData.js
+++ test/server/test_routes/upload/schoolData.js
@@ -1,12 +1,74 @@
+const _ = require('lodash');
 const expect = require('expect');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const path = require('path');
 const request = require('supertest');
+const sinon = require('sinon');
+
+// const routes = require(path.join(process.env.PWD, 'app/server/routes'));
+const app = require(path.join(process.env.PWD, 'app/server/server'))
+  .default;
+
+const { users } = require('../../dbseed/seed');
+const { testRoute } = require('../../testUtils');
 
 describe('/upload/schoolData', () => {
   describe('POST', () => {
+
+    it('should respond with status 401 for an unauthorized user', (done) => {
+      const tests = [
+        {
+          request: {
+            method: 'post',
+            url: encodeURI('/upload/schoolData')
+          },
+          response: {
+            status: 401
+          }
+        }
+      ];
+
+      testRoute(app, tests, done);
+    });
+
     it.skip('should save valid schoolData uploaded as a csv file', () => {});
 
-    it.skip('should return a status 500 error if the schoolData is invalid', () => {});
+    it('should return a status 500 error if the schoolData is invalid', (done) => {
+      // TODO: Test other ways of having invalid data
+      const authHeader = 'JWT ' + jwt.sign(users[3], process.env.SECRET, {
+        expiresIn: 100080
+      });
 
-    it.skip('should return a status 500 error if no data is uploaded', () => {});
+      request(app)
+        .post('/upload/schoolData')
+        .set('Authorization', authHeader)
+        .set('Content-Type', 'multipart/form-data')
+        .attach('file', null)
+        .then((res) => {
+          expect(res.status)
+            .toBe(500);
+          done();
+        })
+        .catch(done);
+
+    });
+
+    it('should return a status 500 error if no data is uploaded', (done) => {
+      const authHeader = 'JWT ' + jwt.sign(users[3], process.env.SECRET, {
+        expiresIn: 100080
+      });
+
+      request(app)
+        .post('/upload/schoolData')
+        .set('Authorization', authHeader)
+        .set('Content-Type', 'multipart/form-data')
+        .then((res) => {
+          expect(res.status)
+            .toBe(500);
+          done();
+        })
+        .catch(done);
+    });
   });
 });
